2254b0bd945d3c6c44cc41db52b50dc0f81a5de3
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2021-02-14 19:14:56 +0000

    Consolidate pages and add 404

---
+++ src/pages/PageNotFound.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {};
+
+const PageNotFound = (props: Props) => {
+    return (
+        <div>
+            This is the 404 page
+        </div>
+    )
+}
+
+export default PageNotFound;
---
+++ src/pages/index.ts
@@ -0,0 +1,5 @@
+export { default as AddFunds } from "./AddFunds";
+export { default as Admin } from "./Admin";
+export { default as Home } from "./Home";
+export { default as PageNotFound } from "./PageNotFound";
+export { default as Rewards } from "./Rewards";
