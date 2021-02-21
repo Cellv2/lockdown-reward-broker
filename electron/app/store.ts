9f1966ef0879518a51224c524990f23e70a7ac8e
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2021-02-21 13:43:05 +0000

    :fire: Cleanup

--- electron/app/store.ts
+++ electron/app/store.ts
@@ -1,15 +1,9 @@
 import Store from "electron-store";
 
-import {
-    accessLogSchema,
-    beneficiariesSchema,
-    rewardsSchema,
-    dbSchema,
-} from "./store.schemas";
+import { dbSchema } from "./store.schemas";
 import accessLogFixture from "./__tests__/access-log.fixture.json";
 import beneficiariesFixture from "./__tests__/beneficiaries.fixture.json";
 import rewardsFixture from "./__tests__/rewards.fixture.json";
-import dbFixture from "./__tests__/db.fixture.json";
 
 const createTestData: boolean = true;
 
@@ -28,37 +22,14 @@ const createTestData: boolean = true;
 // console.log(store.get("unicorn"));
 // //=> undefined
 
-const rewardsStore = new Store({
-    name: "rewards",
-    schema: rewardsSchema,
-});
-
-const beneficiariesStore = new Store({
-    name: "beneficiaries",
-    schema: beneficiariesSchema,
-});
-
-const accessLogStore = new Store({
-    name: "access-log",
-    schema: accessLogSchema,
-});
-
 const dbStore = new Store({
     name: "db",
     schema: dbSchema,
 });
 
 if (createTestData) {
-    // rewardsStore.clear();
-    // rewardsStore.set("data", rewardsFixture);
-    
-    // beneficiariesStore.clear();
-    // beneficiariesStore.set("data", beneficiariesFixture);
-    
-    // accessLogStore.clear();
-    // accessLogStore.set("data", accessLogFixture);
     dbStore.clear();
     dbStore.set("accessLog", accessLogFixture);
-    dbStore.set("beneficaries", beneficiariesFixture)
+    dbStore.set("beneficaries", beneficiariesFixture);
     dbStore.set("rewards", rewardsFixture);
 }
