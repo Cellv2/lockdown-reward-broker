7672907107900f2d8f16d1dd97809a11b7ad9a6b
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2021-02-21 13:41:13 +0000

    Consolidate store schema

--- electron/app/store.ts
+++ electron/app/store.ts
@@ -4,10 +4,12 @@ import {
     accessLogSchema,
     beneficiariesSchema,
     rewardsSchema,
+    dbSchema,
 } from "./store.schemas";
 import accessLogFixture from "./__tests__/access-log.fixture.json";
 import beneficiariesFixture from "./__tests__/beneficiaries.fixture.json";
 import rewardsFixture from "./__tests__/rewards.fixture.json";
+import dbFixture from "./__tests__/db.fixture.json";
 
 const createTestData: boolean = true;
 
@@ -41,13 +43,22 @@ const accessLogStore = new Store({
     schema: accessLogSchema,
 });
 
+const dbStore = new Store({
+    name: "db",
+    schema: dbSchema,
+});
+
 if (createTestData) {
-    rewardsStore.clear();
-    rewardsStore.set("data", rewardsFixture);
-
-    beneficiariesStore.clear();
-    beneficiariesStore.set("data", beneficiariesFixture);
-
-    accessLogStore.clear();
-    accessLogStore.set("data", accessLogFixture);
+    // rewardsStore.clear();
+    // rewardsStore.set("data", rewardsFixture);
+    
+    // beneficiariesStore.clear();
+    // beneficiariesStore.set("data", beneficiariesFixture);
+    
+    // accessLogStore.clear();
+    // accessLogStore.set("data", accessLogFixture);
+    dbStore.clear();
+    dbStore.set("accessLog", accessLogFixture);
+    dbStore.set("beneficaries", beneficiariesFixture)
+    dbStore.set("rewards", rewardsFixture);
 }
